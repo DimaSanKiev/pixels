ac0cd0e6e09c8b752acc7e06f143580fdbc5335f
Dmytro Burdyga <dmytro.burdyga@gmail.com>
2017-03-14 23:43:51 +0200

    Configure datasource and add it to LocalSessionFactoryBean

--- src/main/java/io/dimasan/pixels/config/DataConfig.java
+++ src/main/java/io/dimasan/pixels/config/DataConfig.java
@@ -1,5 +1,6 @@
 package io.dimasan.pixels.config;
 
+import org.apache.tomcat.dbcp.dbcp2.BasicDataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,6 +9,8 @@ import org.springframework.core.env.Environment;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+
+import javax.sql.DataSource;
 
 @Configuration
 @PropertySource("app.properties")
@@ -26,6 +29,17 @@ public class DataConfig {
         LocalSessionFactoryBean sessionFactoryBean = new LocalSessionFactoryBean();
         sessionFactoryBean.setConfigLocation(config);
         sessionFactoryBean.setPackagesToScan(env.getProperty("pixels.entity.package"));
+        sessionFactoryBean.setDataSource(dataSource());
         return sessionFactoryBean;
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        BasicDataSource dataSource = new BasicDataSource();
+        dataSource.setDriverClassName(env.getProperty("pixels.db.driver"));
+        dataSource.setUrl(env.getProperty("pixels.db.url"));
+        dataSource.setUsername(env.getProperty("pixels.db.username"));
+        dataSource.setPassword(env.getProperty("pixels.db.password"));
+        return dataSource;
     }
 }
