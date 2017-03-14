e6bf0c44136a73e4665aeaa1f1f1598f036f4db1
Dmytro Burdyga <dmytro.burdyga@gmail.com>
2017-03-14 02:50:56 +0200

    Add app.properties file and use it with Environment Spring's class

--- src/main/java/io/dimasan/pixels/config/DataConfig.java
+++ src/main/java/io/dimasan/pixels/config/DataConfig.java
@@ -1,19 +1,31 @@
 package io.dimasan.pixels.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 
 @Configuration
+@PropertySource("app.properties")
 public class DataConfig {
+
+    private Environment env;
+
+    @Autowired
+    public void setEnv(Environment env) {
+        this.env = env;
+    }
 
     @Bean
     public LocalSessionFactoryBean sessionFactory() {
         Resource config = new ClassPathResource("hibernate.cfg.xml");
         LocalSessionFactoryBean sessionFactoryBean = new LocalSessionFactoryBean();
         sessionFactoryBean.setConfigLocation(config);
+        sessionFactoryBean.setPackagesToScan(env.getProperty("pixels.entity.package"));
         return sessionFactoryBean;
     }
 }
