183148129d1392deabfa7adbd232fd5d8b8973a3
Dmytro Burdyga <dmytro.burdyga@gmail.com>
2017-03-16 01:41:07 +0200

    Refactor project with Maven

---
+++ src/main/java/io/dimasan/pixels/PixelsApplication.java
@@ -0,0 +1,12 @@
+package io.dimasan.pixels;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PixelsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PixelsApplication.class, args);
+	}
+}
