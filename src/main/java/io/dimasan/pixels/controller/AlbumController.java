a6b4020d5fe1d3b1d3fba5b481c813b8c28352df
Dmytro Burdyga <dmytro.burdyga@gmail.com>
2017-03-17 02:22:57 +0200

    Add album displaying functionality and album template

--- src/main/java/io/dimasan/pixels/controller/AlbumController.java
+++ src/main/java/io/dimasan/pixels/controller/AlbumController.java
@@ -4,6 +4,7 @@ import io.dimasan.pixels.service.AlbumService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -20,5 +21,11 @@ public class AlbumController {
     public String listAlbums(Model model) {
         model.addAttribute("albums", albumService.listAll());
         return "albums";
+    }
+
+    @RequestMapping("/album/{id}")
+    public String getAlbum(@PathVariable Long id, Model model) {
+        model.addAttribute("album", albumService.getById(id));
+        return "album";
     }
 }
